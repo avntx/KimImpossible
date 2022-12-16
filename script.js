a7770b4fdf2f613ef742a47106b093ec4bbdb528
avnee6 <avneetkaur0702@gmail.com>
2022-12-16 15:33:00 +0800

    Updated prototype

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('test')
+let faqButton = document.getElementById('questionSelector')
+
+const clickHandler = () => {
+    console.log('Button is clicked')
+}
+
+faqButton.addEventListener('click', clickHandler)
+
+
+let questionSelector = document.getElementById('questionSelector')
+let answerDiv = document.getElementById('answerDiv')
+
+const revealAnswerHandler = () => {
+
+    if (answerDiv.style.display == 'none') {
+        answerDiv.style.display = 'flex';
+    } else {
+        answerDiv.style.display = 'none';
+    }
+}
+
+questionSelector.addEventListener('click', revealAnswerHandler)
+
+
+let showMoreButton = document.getElementById('FAQ_button')
+let showMoreContent = document.getElementById('showMore_content')
+
+const showMoreQuestionHandler = () => {
+    if (showMoreContent.style.display == 'none') {
+        showMoreContent.style.display = 'block'
+    } else {
+        showMoreContent.style.display = 'none'
+    }
+}
+
+showMoreButton.addEventListener('click', showMoreQuestionHandler)
