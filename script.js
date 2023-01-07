7400eeca3af627652e9e7c7e8304932e92275277
wongtszfung <wongtszfung01@gmail.com>
2023-01-07 22:10:57 +0800

    Fixed most
    Button fixed, copyright added

--- script.js
+++ script.js
@@ -78,37 +78,112 @@ const showMoreQuestionHandler = () => {
 showMoreButton.addEventListener('click', showMoreQuestionHandler)
 console.log('test')
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
 
-  $(window).on("load",function() {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fade").each(function() {
-        /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
-        
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-        } else { //object goes out of view (scrolling up)
-          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+let bookNow_btn = document.getElementById('bookNow_btn')
+let modalContainer = document.getElementById('modalContainer')
+let closeModalBtn = document.getElementById('modal_btn')
+let page = document.getElementById('container')
+
+const displayModal = () => {
+    modalContainer.style.display = 'flex'
+    page.style.opacity = '1'
+}
+
+const closeModal = () => {
+    modalContainer.style.display = 'None'
+    page.style.opacity = '1'
+}
+
+bookNow_btn.addEventListener('click', displayModal)
+closeModalBtn.addEventListener('click', closeModal)
+
+
+
+
+
+let plusAdultTicket = document.getElementById('adult_increase')
+let minusAdultTicket = document.getElementById('adult_decrease')
+
+let adult_counter = 0;
+let adultTix_indiPrice = 69;
+let children_counter = 0;
+let childTix_indiPrice = 49;
+
+let adultTix_totalPrice = 0;
+let childTix_totalPrice = 0;
+
+const plusAdultHandler = () => {
+    adult_counter += 1;
+    document.getElementById('adult_counter').innerHTML = adult_counter
+    document.getElementById('adultType').innerHTML = 'Adult'
+    document.getElementById('adultTix_num').innerHTML = adult_counter
+    adultTix_totalPrice = adult_counter * adultTix_indiPrice
+    document.getElementById('adultTix_price').innerHTML = adultTix_totalPrice
+
+    document.getElementById('tixTotalPrice').innerHTML = childTix_totalPrice + adultTix_totalPrice
+
+}
+
+const minusAdultHandler = () => {
+    adult_counter -= 1;
+    document.getElementById('adult_counter').innerHTML = adult_counter
+
+    document.getElementById('adultTix_num').innerHTML = adult_counter
+    document.getElementById('adultTix_price').innerHTML = adult_counter * adultTix_indiPrice
+    adultTix_totalPrice = adult_counter * adultTix_indiPrice
+    document.getElementById('tixTotalPrice').innerHTML = childTix_totalPrice + adultTix_totalPrice
+
+
+    if (adult_counter <= 0) {
+        adult_counter = 0
+        document.getElementById('adultType').innerHTML = ''
+        document.getElementById('adult_counter').innerHTML = adult_counter
+        document.getElementById('adultTix_num').innerHTML = ''
+        document.getElementById('adultTix_price').innerHTML = ''
+
+        if (adult_counter <= 0 & children_counter <= 0) {
+            document.getElementById('tixTotalPrice').innerHTML = ''
         }
-      });
-    }).scroll(); //invoke scroll-handler on page-load
-  });
+    }
+}
+
+const plusChildHandler = () => {
+    children_counter += 1;
+    document.getElementById('children_counter').innerHTML = children_counter
+    document.getElementById('childType').innerHTML = 'Children'
+
+    document.getElementById('childTix_num').innerHTML = children_counter
+    childTix_totalPrice = children_counter * childTix_indiPrice
+    document.getElementById('childTix_price').innerHTML = childTix_totalPrice
+
+    document.getElementById('tixTotalPrice').innerHTML = childTix_totalPrice + adultTix_totalPrice
+}
+
+const minusChildHandler = () => {
+    children_counter -= 1;
+    document.getElementById('children_counter').innerHTML = children_counter
+    document.getElementById('childTix_num').innerHTML = children_counter
+
+    document.getElementById('childTix_price').innerHTML = children_counter * childTix_indiPrice
+    childTix_totalPrice = children_counter * childTix_indiPrice
+    document.getElementById('tixTotalPrice').innerHTML = childTix_totalPrice + adultTix_totalPrice
+
+    if (children_counter <= 0) {
+        children_counter = 0
+        document.getElementById('children_counter').innerHTML = children_counter
+        document.getElementById('childType').innerHTML = ''
+        document.getElementById('childTix_num').innerHTML = ''
+        document.getElementById('childTix_price').innerHTML = ''
+
+        if (adult_counter <= 0 & children_counter <= 0) {
+            document.getElementById('tixTotalPrice').innerHTML = ''
+        }
+    }
+}
+
+
+window.onload = function() {
+document.getElementById('adult_counter').innerHTML = adult_counter;
+document.getElementById('children_counter').innerHTML = children_counter;
+
+}
